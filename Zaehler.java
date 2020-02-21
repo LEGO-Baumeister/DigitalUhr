41dd0dee0d8c523507873afec4b9ce9816507ca1
LEGO_Baumeister <lukas.severin.breuer@gmail.com>
2020-02-21 15:56:41 +0100

    Method zaehlen() improved

--- Zaehler.java
+++ Zaehler.java
@@ -1,6 +1,6 @@
-
+import java.lang.*;
 /**
- * Write a description of class Zaehler here.
+ * Zählt bis zu einer bestimmten Zahl.
  *
  * @author Lukas Breuer
  * @version 17.02.2020
@@ -9,27 +9,39 @@ public class Zaehler
 {
     private int zeitWert;
     private int zaehlenBis;
+    private int delay;
     
     public Zaehler(int zaehlenBis) {
     zeitWert = 0;
     this.zaehlenBis = zaehlenBis;
-    zaehlen(zaehlenBis);
+    zaehlen(zaehlenBis, 20);
     }
     
     public void reset() {
         setZeitWert(0);
     }
     
+    public void sleep(long delay) {
+	try {
+		Thread.sleep(delay);
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+    }
     
-    public int zaehlen(int zaehlenBis) {
+    private void zeitAnzeige() {
         
-        while(zeitWert < zaehlenBis) {
-            if(zeitWert == zaehlenBis - 1) {
-                reset();
-            } else {
-              System.out.println(zeitWert);
-              zeitWert++;
-            }
+    }
+    
+    
+    public int zaehlen(int zaehlenBis, long delay) {
+        
+        for(int i = zeitWert; i <= zaehlenBis - 1; i++) {
+            if(i < 10) {
+                System.out.println("0" + i);
+            } else
+                System.out.println(i);
+            sleep(delay);
         }
         return zeitWert;
     }
