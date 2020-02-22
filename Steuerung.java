a61d5a5509a5693ad6efed19f1358bb58efb3027
LEGO_Baumeister <lukas.severin.breuer@gmail.com>
2020-02-22 15:40:12 +0100

    Added class "Steuerung" and created 3 objects

---
+++ Steuerung.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Steuerung here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Steuerung
+{   
+    Zaehler sekunden = new Zaehler(60);
+    Zaehler minuten = new Zaehler(60);
+    Zaehler stunden = new Zaehler(24);
+}
